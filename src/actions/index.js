bfa29334f65dc1bbbbec6ca5685b880def5950c9
sachins <39sachen@gmail.com>
2018-05-30 12:49:26 +0300

    weather reducer added and request promise converted into response data using middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API_KEY ="b6907d289e10d714a6e88b30761fae22";
-const ROOT_URL = `http://api.openweathermao.org/data/2.5/forcast?appid=${API_KEY}`;
+const API_KEY ="b9963445d60cb2e5fdcc39af32367ef1";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+   
 
     return {
         type: FETCH_WEATHER,
