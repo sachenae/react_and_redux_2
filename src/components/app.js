c473c7874e1cf72103e88c2ffba8a2ff0eef76ed
sachins <39sachen@gmail.com>
2018-05-30 14:06:07 +0300

    passing props dta and generating graph using sparklines

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 
 export default class App extends Component {
@@ -7,6 +8,7 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
