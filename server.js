a2a0814c364ad89f46cb975c684c2612df555e2c
sachins <39sachen@gmail.com>
2018-06-05 14:02:17 +0300

    redo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('*', (req,res)=>{
+app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
